a0de7ec0bef19dfc79b3a7e9a715c85884ead516
Joan Barrull Ribalta <giria@users.noreply.github.com>
2022-08-09 15:45:49 +0100

    bridge

---
+++ Adapter.playground/Pages/Bridge.xcplaygroundpage/Contents.swift
@@ -0,0 +1,24 @@
+//: [Previous](@previous)
+
+
+/*:
+ 
+ # Bridge
+ 
+ Separates common and different functionality into different hierarchies.
+ 
+ 
+Example, given we want to provide support for light and dark themes:
+ 
+ ![example](themes.png)
+ 
+ This does not scales well and provokes a "explosion class hierrachies" problem.
+ The solution is to use the bridge pattern. The bridge pattern stops the proliferation of sub classes.
+ Besides, it introduces a separation that lets us modify either side independently to add a new theme.
+ 
+ */
+import Foundation
+
+var greeting = "Hello, playground"
+
+//: [Next](@next)
