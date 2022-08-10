e1bacd84e4f55f3434076210f359bc74bc491320
Joan Barrull Ribalta <giria@users.noreply.github.com>
2022-08-10 19:12:38 +0100

    facade

---
+++ Adapter.playground/Pages/Facade.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+//: [Previous](@previous)
+/*:
+ 
+ ## Facade
+ 
+ Hides the complexity of underlying components.
+ Clients use the Façade instead of the complex subsystems
+ 
+ 
+ ![facade](facade.png)
+ 
+ 
+ ## When to use facade?
+ 
+ - Clients don't need all features of a library/types.
+ - Isolate client from changes
+ 
+ */
+import Foundation
+
+var greeting = "Hello, playground"
+
+//: [Next](@next)
