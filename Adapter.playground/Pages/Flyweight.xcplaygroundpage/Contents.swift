ebe18ee877a64d3202ad3edb434dff4c221a7142
Joan Barrull Ribalta <giria@users.noreply.github.com>
2022-08-11 08:38:37 +0100

    flyweigth

---
+++ Adapter.playground/Pages/Flyweight.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+//: [Previous]
+
+/*:
+ 
+ 
+ # Flyweight
+ 
+ - Reduces memory usage by sharing common objects across multiple contexts.
+ - Eliminates redundant object creation
+ - Shares common data instead of recreating it
+  
+ ![flyweight](flyweight.png)
+
+ The UML diagram is:
+
+ ![flyweightUML](flyweightUML.png)
+ */
+
+ 
+ 
+
+
+//: [Next](@next)
